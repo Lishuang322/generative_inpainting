790c0cf482081692ec87858bf0ba6542c3e2d67f
Lishuang322 <hulishuang@yeah.net>
2018-12-18 16:43:24 +0800

    inpaint images in large quantities

---
+++ inpaint.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#directory of dataset
+dir="/data/data-hulishuang/img-dataset/oxford_smeared/white/t85_p400"
+picture=$dir/"picture"
+mask=$dir/"mask"
+for file in $picture/*
+do
+    #delete the prefix(path) get image name 
+    img=${file##*/}
+    #get the corresponding absolute path of mask
+    mask=${mask}/$img
+    checkpoint_d="/data/data-hulishuang/project-data/generative_inpainting/release_places2_256"
+    output_dir="/data/data-hulishuang/img-dataset/oxford_smeared/inpaint/t85_p400_places"
+    if [ ! -d $output_dir ]
+    then
+        mkdir $output_dir
+    fi
+    output_image=$output_dir/${img}
+    
+    python test.py --image $file --mask $mask --output $output_image --checkpoint_dir $checkpoint_d
+done
+
+#change dir, checkpoint_d, output_dir before running
+
